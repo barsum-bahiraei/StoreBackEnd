5514b99dbb2d1467c89e4b15818d86be34706495
Amin Bahiraei <amin.bahiraei.75@gmail.com>
2024-08-27 15:51:02 +0330

    change module

--- src/main.ts
+++ src/main.ts
@@ -19,9 +19,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  app.useGlobalPipes(new ValidationPipe({
-
-  }));
+  app.useGlobalPipes(new ValidationPipe({}));
 
   await app.listen(3000);
 
