176229ab056bf8e7ad257fdc8c095dce7bb69b69
Amin Bahiraei <amin.bahiraei.75@gmail.com>
2024-08-30 12:22:16 +0330

    change module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,15 +2,19 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([UserEntity]),
     JwtModule.registerAsync({
-      imports: [ConfigService],
+      imports: [ConfigModule],
+      inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
         return {
-          isGlobal: true,
+          global: true,
           signOptions: { expiresIn: '1d' },
           secret: configService.get<string>('SECRET_KEY'),
         };
